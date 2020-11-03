8fd8e159ff9b84f4da3da95a004c48bf2dab9502
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2020-11-03 17:45:13 +0100

    Update batch.py

--- batch.py
+++ batch.py
@@ -3,16 +3,17 @@ import argparse
 from apache_beam.options.pipeline_options import PipelineOptions
 from sys import argv
 
-# general informations regarding the project & the Schema for BigQuery
 PROJECT_ID = 'sandbox-mscherding'
 SCHEMA = 'show_id:INTEGER,type:STRING,title:STRING,director:STRING,country:STRING,release_year:FLOAT,rating:FLOAT,duration:FLOAT'
 
-# filters out records that don't have an information
 def discard_incomplete(data):
+    """Filters out records that don't have an information."""
+   def discard_incomplete(data):
     return len(data['show_id']) > 0 and len(data['type']) > 0 and len(data['title']) > 0 and len(data['director']) > 0 and len(data['country']) > 0 and len(data['release_year']) > 0 and len(data['rating']) > 0 and len(data['duration']) > 0
 
-# converts string values to their appropriate type
+
 def convert_types(data):
+    """Converts string values to their appropriate type."""
     data['show_id'] = int(data['show_id']) if 'show_id' in data else None
     data['type'] = str(data['type']) if 'type' in data else None
     data['title'] = str(data['title']) if 'title' in data else None
@@ -23,22 +24,21 @@ def convert_types(data):
     data['duration'] = float(data['duration']) if 'duration' in data else None
     return data
 
-# delete the unwanted columns
 def del_unwanted_cols(data):
+    """Delete the unwanted columns"""
     del data['cast']
     del data['date_added']
     del data['listed_in']
     del data['description']
     return data
 
-# main for function call
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     known_args = parser.parse_known_args(argv)
 
-    pipeline = beam.Pipeline(options=PipelineOptions())
-    
+    p = beam.Pipeline(options=PipelineOptions())
+
     (p | 'ReadData' >> beam.io.ReadFromText('gs://nf-bucket-test/batch/netflix_titles.csv', skip_header_lines =1)
        | 'SplitData' >> beam.Map(lambda x: x.split(','))
        | 'FormatToDict' >> beam.Map(lambda x: {"show_id": x[0], "type": x[1], "title": x[2], "director": x[3], "country": x[4], "release_year": x[5], "rating": x[6], "duration": x[7]}) 
